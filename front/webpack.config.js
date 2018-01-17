184e0a2b774996fdd3c042969faef325ab7b5f76
t-kusakabe <ttmac0704@gmail.com>
2018-01-17 14:03:31 +0900

    Create webpack.config.js

---
+++ front/webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path')
+
+module.exports = {
+  entry: './src/app.js',
+  output: {
+    path: path.resolve('dist/'),
+    filename: 'bundle.js'
+  },
+  devtool: 'inline-source-map',
+  module: {
+    loaders: [
+      {
+        test: /\.js(x?)$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      }
+    ]
+  },
+  devServer: {
+    historyApiFallback: true,
+    contentBase: path.resolve(__dirname, './dist'),
+    inline: true,
+    port: 8080,
+    host: '0.0.0.0',
+  }
+}
