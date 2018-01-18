3e46822c047bc9bd4277870dbe40a0ac3e363189
t-kusakabe <ttmac0704@gmail.com>
2018-01-18 12:09:01 +0900

    Create websocket

--- app.rb
+++ app.rb
@@ -1,8 +1,33 @@
 require 'sinatra'
 require 'sinatra/reloader'
+# websocket
+require 'sinatra-websocket'
+require 'pp'
+
+set :sockets, []
 
 #################
 ## Controller
 ################
-get '/index.html' do
+get '/test.html' do
+end
+
+get '/websocket' do
+  if request.websocket?
+    request.websocket do |ws|
+      ws.onopen do
+        settings.sockets << ws
+      end
+
+      ws.onmessage do |msg|
+        settings.sockets.each do |s|
+          s.send(msg)
+        end
+      end
+
+      ws.onclose do
+        settings.sockets.delete(ws)
+      end
+    end
+  end
 end
